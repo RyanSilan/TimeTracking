96d80d35f3faf0dcb55d933e85c384053fea8d8e
Ryan Silan <ryan.silan@gmail.com>
2018-12-10 18:54:41 -0500

    Add project files.

---
+++ CET_TimeLogging/SQL/Connection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CET_TimeLogging.SQL
+{
+    class Connection
+    {
+         public Connection()
+        {
+
+            try
+            {
+                Console.WriteLine("Attempting to insert data");
+
+                string query = "INSERT INTO dbo.calls(Time, Date) VALUES(@TimeValue, @DateValue)"; 
+
+                using (SqlConnection con = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Database=CET;Trusted_Connection=True;"))
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    string hr = "12"; 
+                    string min = "10"; 
+                    string time = hr + ":" + min;
+
+                    // set the parameter value
+                    cmd.Parameters.Add("@TimeValue", SqlDbType.Time).Value = TimeSpan.Parse(time);
+                    cmd.Parameters.Add("@DateValue", SqlDbType.Date).Value = DateTime.Parse("12/10/2018"); 
+
+                    // open connection, execute your INSERT, close connection
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception Occre while creating table:" + e.Message + "\t" + e.GetType());
+            }
+            
+
+        }
+    }
+}
